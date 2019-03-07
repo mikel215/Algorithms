9d6a32eff98d773026eff6091d44177800fbef42
mikel <mikelmayorga08@gmail.com>
2019-03-06 23:23:14 -0800

    added computeMostEfficientRoute function which incorporates Dijkstras algorithm into an MST

--- PA3/PA3/main.cpp
+++ PA3/PA3/main.cpp
@@ -56,32 +56,70 @@ vector<string> getDestinations(string filename)
 
 }
 
-int getTotalTime(vector<Edge> mst)
+vector<Edge> computeMostEfficientRoute(CityGraph graph, vector<string> destinations)
+{
+	// create empty graph
+	CityGraph new_graph{};
+
+	// add destinations as the ONLY vertices
+	for (auto i : destinations)
+	{
+		new_graph.addVertex(i);
+	}
+
+	// compute shortest path from each vertex using Dijkstra's algorithm, using 
+	// the original graph
+	for (int i = 0; i < destinations.size(); i++)
+	{
+		// exit condition (last element of vector)
+		if (i + 1 >= destinations.size())
+		{
+			// connect final element to the first
+			unordered_map<string, int> distances = graph.computeShortestPath(destinations[i]);
+			new_graph.connectVertex(destinations[i], destinations[0], distances[destinations[0]], true);
+			break;
+		}
+		// run Dijkstra's alg at each vertex
+		unordered_map<string, int> distances = graph.computeShortestPath(destinations[i]);
+		new_graph.connectVertex(destinations[i], destinations[i+1], distances[destinations[i+1]], true);
+
+
+	}
+	// build MST on the new graph/tree
+	vector<Edge> routes = new_graph.computeMinimumSpanningTree(destinations[0]);
+
+	return routes;
+
+}
+
+void getRouteTime(vector<Edge> mst)
 {
 	int i = 0;
 	for (auto edge : mst)
 	{
 		i += edge.weight;
 	}
-	return i;
+
+	cout << "Total transit time: " << i << " minutes" << endl;
 }
 
 int main(void)
 {
 	cout << "***Route Planner***" << endl;
 	//Example of how to parse a CSV file for graph building
-	CsvStateMachine csm{ "map1.txt" };
+	CsvStateMachine csm{ "map2.txt" };
 	vector<vector<string>> data = csm.processFile();
 
 	CityGraph graph{};
 	buildMST(graph, data);
 
-	vector<string> deliveries = getDestinations("deliveries1.txt");
+	vector<string> deliveries = getDestinations("deliveries2.txt");
 
 	// compute mst from starting vertex
-	vector<Edge> mst = graph.computeMinimumSpanningTree(deliveries[0]);
+	vector<Edge> mst = computeMostEfficientRoute(graph, deliveries);
 
-	cout << "Total transit time: " << getTotalTime(mst) << " minutes" << endl;
+	getRouteTime(mst);
+
 
 
 	/*
